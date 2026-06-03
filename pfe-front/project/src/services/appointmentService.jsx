870586cdb37db3b7137053d3f74a22a665c3968c
douae homirat <douaehomirat@gmail.com>
2026-06-03 22:27:23 +0100

    fix api socket

--- pfe-front/project/src/services/appointmentService.jsx
+++ pfe-front/project/src/services/appointmentService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/appointments";
+const API_URL = "https://gestion-rendezvous-commune-production.up.railway.app/api/appointments";
 
 /* ================= CRUD ================= */
 
